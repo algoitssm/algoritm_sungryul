465f64f0b8630e8a092ed44b9e9b55d3d644a116
sungryul <airtrack03@naver.com>
2021-08-11 23:30:43 +0900

    0811 Algorithm Study

--- Baekjoon/1343_Greedy_폴리오미노.py
+++ Baekjoon/1343_Greedy_폴리오미노.py
@@ -1,29 +1,19 @@
-## ✅1343
-
-- 통과 코드
-
-``` python
 board = input()
 
-board = board.replace('XXXX', 'AAAA')
-board = board.replace('XX', 'BB')
+# board = board.replace("XXXX", "AAAA")
+# board = board.replace("XX", "BB")
 
-if 'X' in board:
-    print(-1)
-else:
-    print(board)
-```
+# if "X" in board:
+#     print(-1)
+# else:
+#     print(board)
 
-- 의문 코드 ➡ 해결!
-
-```python
-board = input()
+poly = ["AAAA", "BB"]
 
 cnt = 0
 result = []
 
 if len(board) <= 1:
-    # 입력 '.'일 경우가 마지막 테스트 케이스
     if board == ".":  # 길이가 1이하일 때 무조건 -1 하려다가 계속 오답...
         print(".")
     else:
@@ -31,7 +21,7 @@ if len(board) <= 1:
 
 else:
     for char in board:
-        if char == 'X':
+        if char == "X":
             cnt += 1
             if cnt == 2:
                 result.append(poly[1])
@@ -44,13 +34,10 @@ else:
                 break
             else:
                 cnt = 0
-                result.append('.')
-    
+                result.append(".")
     if result == -1:
         print(result)
     elif cnt == 1 or cnt == 3:
         print(-1)
     else:
-        print(''.join(result))
-```
-
+        print("".join(result))
