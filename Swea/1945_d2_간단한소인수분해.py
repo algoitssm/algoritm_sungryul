25e081ad7ce2f5a5cfc1b888505c0928716be690
sungryul <airtrack03@naver.com>
2021-08-12 11:10:39 +0900

    SWEA 1945 Modified

--- Swea/1945_d2_간단한소인수분해.py
+++ Swea/1945_d2_간단한소인수분해.py
@@ -1,17 +1,18 @@
+# N이 2, 3, 5, 7, 11의 지수의 곱으로 만들어 질 수 있는 a, b, c, d, e 값 구하는 문제
 T = int(input())
 
-for tc in range(1, T+1):
+for tc in range(1, T + 1):
     N = int(input())
     prime_nums = [2, 3, 5, 7, 11]
-    result = []
+    result = []  # a, b, c, d, e 저장할 리스트
 
-    for prime_num in prime_nums:
+    for prime_num in prime_nums:  # 소수 리스트의 소수 하나씩 체크
         cnt = 0
-        while N % prime_num == 0:
+        while N % prime_num == 0:  # 소수로 계속 나눠서 소수 곱이 사라지면 나머지가 생기므로 while 종료
             cnt += 1
             N //= prime_num
         result.append(cnt)
-    
-    ans = ' '.join(tuple(map(str, result)))
 
-    print(f'#{tc} {ans}')
+    ans = " ".join(list(map(str, result)))
+
+    print("#{} {}".format(tc, ans))
