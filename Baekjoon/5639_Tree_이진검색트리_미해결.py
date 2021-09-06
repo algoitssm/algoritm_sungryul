d06888033992a4eb5c1c1671d12bad4a03feb73b
sungryul <airtrack03@naver.com>
2021-09-06 19:08:54 +0900

    BAEKJOON 5639 Solving

--- Baekjoon/5639_Tree_이진검색트리_미해결.py
+++ Baekjoon/5639_Tree_이진검색트리_미해결.py
@@ -3,55 +3,84 @@
 setrecursionlimit 쓰면 Pypy는 1%에서 메모리 초과. python3는 27% 쯤에서 시간초과
 setrecursionlimit 쓰면 Pypy Recurtion Error. Python3는 틀렸습니다
 """
+
 import sys
 
 sys.setrecursionlimit(10 ** 6)
 input = sys.stdin.readline
 
 
-class Node:
-    def __init__(self, value=None):
-        self.value = value
-        self.left_child = None
-        self.right_child = None
+# class Node:
+#     def __init__(self, value=None):
+#         self.value = value
+#         self.left_child = None
+#         self.right_child = None
 
 
-class BST:
-    def __init__(self):
-        self.root = None
+# class BST:
+#     def __init__(self):
+#         self.root = None
 
-    def insert(self, value):
-        if self.root == None:
-            self.root = Node(value)
-        else:
-            self._insert(value, self.root)
+#     def insert(self, value):
+#         if self.root == None:
+#             self.root = Node(value)
+#         else:
+#             self._insert(value, self.root)
 
-    def _insert(self, value, cur_node):
-        if value < cur_node.value:
-            if cur_node.left_child == None:
-                cur_node.left_child = Node(value)
-            else:
-                self._insert(value, cur_node.left_child)
-        elif value > cur_node.value:
-            if cur_node.right_child == None:
-                cur_node.right_child = Node(value)
-            else:
-                self._insert(value, cur_node.right_child)
+#     def _insert(self, value, cur_node):
+#         if value < cur_node.value:
+#             if cur_node.left_child == None:
+#                 cur_node.left_child = Node(value)
+#             else:
+#                 self._insert(value, cur_node.left_child)
+#         elif value > cur_node.value:
+#             if cur_node.right_child == None:
+#                 cur_node.right_child = Node(value)
+#             else:
+#                 self._insert(value, cur_node.right_child)
 
-    def post_order(self, node):
-        if node.left_child:
-            self.post_order(node.left_child)
-        if node.right_child:
-            self.post_order(node.right_child)
-        print(node.value)
+#     def post_order(self, node):
+#         if node.left_child:
+#             self.post_order(node.left_child)
+#         if node.right_child:
+#             self.post_order(node.right_child)
+#         print(node.value)
 
 
-bst = BST()
+# bst = BST()
+
+# while True:
+#     try:
+#         bst.insert(int(input()))
+#     except:
+#         break
+
+# bst.post_order(bst.root)
+
+
+def post_order(start, end):
+    if start >= end:
+        return
+    root = data[start]
+    left_node_idx = start + 1
+    right_node_idx = end + 1
+    for i in range(start + 1, end):
+        if data[i] > data[start]:
+            right_node_idx = i
+            break
+    post_order(left_node_idx, right_node_idx)
+    post_order(right_node_idx, end)
+    print(data[start])
+
+
+data = []
+N = 0  # 노드 개수
 
 while True:
     try:
-        bst.insert(int(input()))
+        data.append(int(input()))
+        N += 1
     except:
         break
 
-bst.post_order(bst.root)
+post_order(0, N)
